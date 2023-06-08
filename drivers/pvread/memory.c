f0433f83f8342207ddbb162f1dd90f0a84b064ba
zY2384 <2597665307@qq.com>
2023-06-08 17:35:37 +0800

    hello

--- drivers/pvread/memory.c
+++ drivers/pvread/memory.c
@@ -13,7 +13,7 @@
 
 extern struct mm_struct *get_task_mm(struct task_struct *task);
 
-#if(LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 61))
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 61)
 extern void mmput(struct mm_struct *);
 
 phys_addr_t translate_linear_address(struct mm_struct* mm, uintptr_t va) {
--- drivers/pvread/process.c
+++ drivers/pvread/process.c
@@ -9,7 +9,7 @@
 
 extern struct mm_struct *get_task_mm(struct task_struct *task);
 
-#if(LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 61))
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 61)
 extern void mmput(struct mm_struct *);
 #endif
 
